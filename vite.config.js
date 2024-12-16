10d39dd89959fc0d14a578a6c4ec6eea927e2b9b
Juan Pedro Iriarte Paolina <iriarte.juan@gmail.com>
2024-12-16 11:00:55 +0100

    chore: optimize project configuration for production

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,65 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { splitVendorChunkPlugin } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    splitVendorChunkPlugin()
+  ],
   build: {
-    outDir: 'build',
-    sourcemap: true,
+    outDir: 'dist',
+    sourcemap: false, // Deshabilitamos sourcemaps en producción para mejor rendimiento
     minify: 'terser',
     terserOptions: {
       compress: {
         drop_console: true,
-        drop_debugger: true
+        drop_debugger: true,
+        pure_funcs: ['console.log', 'console.info', 'console.debug', 'console.trace'],
+      },
+      mangle: true,
+      format: {
+        comments: false
       }
     },
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          ui: ['styled-components', 'framer-motion'],
-          icons: ['react-icons']
-        }
+          'vendor-react': ['react', 'react-dom', 'react-router-dom'],
+          'vendor-ui': ['styled-components', 'framer-motion'],
+          'vendor-utils': ['date-fns', 'react-intersection-observer'],
+          'vendor-icons': ['react-icons', 'lucide-react', '@fortawesome/fontawesome-free']
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
       }
-    }
+    },
+    target: 'es2015',
+    cssCodeSplit: true,
+    assetsInlineLimit: 4096, // 4kb
+    chunkSizeWarningLimit: 500
   },
   server: {
     port: 3000,
-    open: true
+    host: true,
+    cors: true,
+    compression: true
   },
   preview: {
-    port: 3000
+    port: 3000,
+    host: true,
+    cors: true,
+    compression: true
+  },
+  optimizeDeps: {
+    include: [
+      'react',
+      'react-dom',
+      'react-router-dom',
+      'styled-components',
+      'framer-motion'
+    ]
   }
 })
